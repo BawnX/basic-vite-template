05e54c1fa1da4d77a84af574d4460dadd274022a
antonio-ans <89044721+antonio-ans@users.noreply.github.com>
2021-12-31 17:43:01 -0300

    chore: update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
-import { createApp } from 'vue'
+import { ViteSSG } from 'vite-ssg'
+import generatedRoutes from 'virtual:generated-pages'
+import { setupLayouts } from 'virtual:generated-layouts'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
 import './index.css'
 
-createApp(App).mount('#app')
+const routes = setupLayouts(generatedRoutes)
+
+export const createApp = ViteSSG(
+    App,
+    { routes, base: import.meta.env.BASE_URL },
+    (ctx) => {
+        // install all modules under `modules/`
+        ctx.app.use(createPinia())
+        Object.values(import.meta.globEager('./modules/*.ts')).map(i => i.install?.(ctx))
+    },
+)
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,106 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Pages from 'vite-plugin-pages'
+import Layouts from 'vite-plugin-vue-layouts'
+import AutoImport from 'unplugin-auto-import/vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import Components from 'unplugin-vue-components/vite'
+import Markdown from 'vite-plugin-md'
+import Prism from 'markdown-it-prism'
+import LinkAttributes from 'markdown-it-link-attributes'
+
+import path from 'path'
+
+const markdownWrapperClasses = 'prose prose-sm m-auto text-left'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+    resolve: {
+        alias: {
+            '~/': `${path.resolve(__dirname, 'src')}/`,
+        }
+    },
+    plugins: [
+        vue({
+            include: [/\.vue$/, /\.md$/],
+        }),
+        Pages({
+            extensions: ['vue', 'md'],
+        }),
+        Layouts(),
+        AutoImport({
+            imports: [
+                'vue',
+                'vue-router',
+                '@vueuse/head',
+                '@vueuse/core',
+                'vitest',
+            ],
+            dts: 'src/auto-imports.d.ts',
+        }),
+        Components({
+            // allow auto load markdown components under `./src/components/`
+            extensions: ['vue', 'md'],
+
+            // allow auto import and register components used in markdown
+            include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+
+            // custom resolvers
+            resolvers: [
+                // auto import icons
+                // https://github.com/antfu/unplugin-icons
+                IconsResolver({
+                    componentPrefix: '',
+                    // enabledCollections: ['carbon']
+                }),
+            ],
+
+            dts: 'src/components.d.ts',
+        }),
+        Icons({
+            autoInstall: true,
+        }),
+        Markdown({
+            wrapperClasses: markdownWrapperClasses,
+            headEnabled: true,
+            markdownItSetup(md) {
+                // https://prismjs.com/
+                // @ts-expect-error types mismatch
+                md.use(Prism)
+                // @ts-expect-error types mismatch
+                md.use(LinkAttributes, {
+                    pattern: /^https?:\/\//,
+                    attrs: {
+                        target: '_blank',
+                        rel: 'noopener',
+                    },
+                })
+            },
+        }),
+    ],
+    ssgOptions: {
+        script: 'async',
+        formatting: 'minify',
+    },
+
+    optimizeDeps: {
+        include: [
+            'vue',
+            'vue-router',
+            '@vueuse/core',
+            '@vueuse/head',
+        ],
+        exclude: [
+            'vue-demi',
+        ],
+    },
+
+    test: {
+        include: ['test/**/*.test.ts'],
+        environment: 'jsdom',
+        deps: {
+            inline: ['@vue', '@vueuse', 'vue-demi'],
+        },
+    },
 })
